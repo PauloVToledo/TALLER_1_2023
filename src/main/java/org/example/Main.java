bb02638752fea42fa12f5f87a5b3ad7946cb3f3e
Paulo Vicente <paulovicentetoledosalgado@gmail.com>
2023-09-13 17:30:07 -0300

    agregué un método en la rama secundaria.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println("Probando primer commit de main");
+
+        mostrarletras();
+    }
+
+    public static void mostrarletras(){
+        System.out.println("muestras del primer commit parte 1.");
+
+    }
+}
