ca4eb91c738447b8c6a7636fbd8ab2359965e896
Paulo Vicente <paulovicentetoledosalgado@gmail.com>
2023-09-13 17:49:36 -0300

    Borré métodos del main y lo depuré.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,5 @@
 package org.example;
-import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Aquí pienso colocar el menú que llame a los métodos.");
